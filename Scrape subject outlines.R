9dc19b396c986d3a68c2ec9adb62471389237eb5
msp203 <msp203@yahoo.com.au>
2018-07-20 08:22:14 +1000

    made changes

--- Scrape subject outlines.R
+++ Scrape subject outlines.R
@@ -6,17 +6,19 @@ library(tidyverse)
 
 #create function to 
 url <- function(subject_code){
-    paste0("http://handbook.uts.edu.au/subjects/", subject_code, ".html") 
+    paste0("http://handbook.uts.edu.au/subjects/details/", subject_code, ".html") 
 }
 
 urls <- url(c(36100, 36103, 36106, 36102, 36104, 36101, 36109, 36105))
 
 xmls <- urls %>% 
-    map(read_html) #%>% 
-    #map(html_node, ".store-details__address") %>% 
-    #map_chr(html_text)#
+    map(read_html) %>% 
+    map(html_node, "h1") %>% 
+    map_chr(html_text)
 
-map(html_node()
+xmls
+
+
 
 
 
